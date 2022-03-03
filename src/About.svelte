d99cfa609ecd715d282e25e3dedacd4f317556fb
Harrison <h.alexander.barnett@gmail.com>
2022-03-02 21:48:22 -0500

    sketching out intersection observer

--- src/About.svelte
+++ src/About.svelte
@@ -4,7 +4,7 @@
 
 <main>
     <div class='about'>
-        <div class='about-card reduce'>
+        <div class='about-card lazy reduce'>
             <div class='card-copy'>
                 <h2>reduce</h2>
                 <p>We want to help you make thoughtful decisions about the things you buy, so we chose to source our products from vendors who share our goal of reducing the amount of packaging material to the bare minimum. Let's work together to make our consumption a little less conspicuous.</p>
@@ -12,7 +12,7 @@
             <img src='' alt=''>
         </div>
 
-        <div class='about-card reuse'>
+        <div class='about-card lazy reuse'>
             <div class='card-copy'>
                 <h2>reuse</h2>
                 <p>An oft-overlooked aspect of living sustainably is simply reusing items that may appear spent. Have you grown fond the soap pump that you've been using for the past month? Hold onto it! Your soap doesn't care what container it lives in, and neither should you. If you intend to start fresh, know that we can provide you with sustainably-sourced reusable containers.</p>
@@ -20,7 +20,7 @@
             <img src='' alt=''>
         </div>
 
-        <div class='about-card replenish'>
+        <div class='about-card lazy replenish'>
             <div class='card-copy'>
                 <h2>replenish</h2>
                 <p>Feel good about the things that you bring into your home. Replenish your conscience by making sustainability a part of your day-to-day. We can even save you the drive, as we'll show up to your doorstep to deliver your refills using the reusable container of your choice.</p>
@@ -29,6 +29,28 @@
         </div>
 
     </div>
+    <!-- Intersection Observer -->
+    <script>
+        // intersection observer 'lazily loads' the cards as they scroll into viewport
+        const options = {
+            threshold: .75
+        }
+        let cardObserver = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    let lazyCard = entry.target
+                    lazyCard.classList.remove('lazy')
+                    lazyCard.classList.add('loaded')
+                    paletteObserver.unobserve(lazyCard)
+                }
+            })
+        }, options)
+
+		const cards = document.querySelectorAll('.about-card')
+        cards.forEach(card => {
+            cardObserver.observe(card)
+        })
+    </script>
 </main>
 
 <style>
