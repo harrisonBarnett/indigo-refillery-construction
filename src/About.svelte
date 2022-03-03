651e76cfafda888dbcb780e613d6de2e8b2ee9b0
Harrison <h.alexander.barnett@gmail.com>
2022-03-02 21:20:44 -0500

    styling cards and header

---
+++ src/About.svelte
@@ -0,0 +1,58 @@
+<script>
+
+</script>
+
+<main>
+    <div class='about'>
+        <div class='about-card reduce'>
+            <div class='card-copy'>
+                <h2>reduce</h2>
+                <p>We want to help you make thoughtful decisions about the things you buy, so we chose to source our products from vendors who share our goal of reducing the amount of packaging material to the bare minimum. Let's work together to make our consumption a little less conspicuous.</p>
+            </div>
+            <img src='' alt=''>
+        </div>
+
+        <div class='about-card reuse'>
+            <div class='card-copy'>
+                <h2>reuse</h2>
+                <p>An oft-overlooked aspect of living sustainably is simply reusing items that may appear spent. Have you grown fond the soap pump that you've been using for the past month? Hold onto it! Your soap doesn't care what container it lives in, and neither should you. If you intend to start fresh, know that we can provide you with sustainably-sourced reusable containers.</p>
+            </div>
+            <img src='' alt=''>
+        </div>
+
+        <div class='about-card replenish'>
+            <div class='card-copy'>
+                <h2>replenish</h2>
+                <p>Feel good about the things that you bring into your home. Replenish your conscience by making sustainability a part of your day-to-day. We can even save you the drive, as we'll show up to your doorstep to deliver your refills using the reusable container of your choice.</p>
+            </div>
+            <img src ='' alt=''>
+        </div>
+
+    </div>
+</main>
+
+<style>
+    .about {
+        margin: 8rem 0;
+        display: flex;
+        flex-direction: column;
+    }
+    .about-card {
+        border: 1px solid red;
+        width: 75%;
+        margin: 4rem 0;
+        padding: 1rem;
+        height: 50vh;
+        display: flex;
+    }
+    .about-card img {
+        height: 100%;
+        aspect-ratio: 1/1;
+        background: pink;
+    }
+    .reuse {
+        margin-left: auto;
+        flex-direction: row-reverse;
+        text-align: right;
+    }
+</style>
--- src/Splash.svelte
+++ src/Splash.svelte
@@ -1,4 +1,5 @@
 <script>
+    import About from './About.svelte'
     
 // TODO:
 // sketch 'about' cards X
@@ -12,37 +13,13 @@
 <main>
     <div class='header'>
         <h1>The Wild Indigo Refillery</h1>
+        <h2>Purveyor of Mobile Sustainable Consumables</h2>
+
         <img class='header-img' src="../static/images/logo-outline.png" alt="a line illustration of an indigo flower">
-        <h2>A Sustainable Consumables Market</h2>
+        <p>Greenville, SC</p>
     </div>
 
-    <div class='about'>
-
-        <div class='about-card reduce-card'>
-            <div class='card-copy'>
-                <h2>reduce</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-            <img src='' alt=''>
-        </div>
-
-        <div class='about-card reuse-card'>
-            <div class='card-copy'>
-                <h2>reuse</h2>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <img src='' alt=''>
-        </div>
-
-        <div class='about-card replenish-card'>
-            <div class='card-copy'>
-                <h2>replenish</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-            <img src ='' alt=''>
-        </div>
-
-    </div>
+    <About />
 
     <div class='image-break'>
     </div>
@@ -57,24 +34,26 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        gap: 4rem;
+        gap: 2rem;
     }
-    .about-card {
-        border: 1px solid red;
-        margin: 4rem 0;
-        padding: 1rem;
-        height: 50vh;
-        display: flex;
+    .header p {
+        position: relative;
     }
-    .about-card img {
-        height: 100%;
-        aspect-ratio: 1/1;
-        background: pink;
+    .header p::before, .header p::after {
+        content: '';
+        position: absolute;
+        height: 1px;
+        width: 50%;
+        background: black;
+        top: 50%;
     }
-    .reuse-card {
-        flex-direction: row-reverse;
-        text-align: right;
+    .header p::before {
+        right: 110%;
     }
+    .header p::after {
+        left: 110%;
+    }
+    
     .image-break {
         height: 100vh;
         width: 100%;
